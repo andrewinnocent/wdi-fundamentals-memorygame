5484235b9bc84ed62ea33ff0fac0f62635d6c959
Andrew Innocent <innocent.andrew@gmail.com>
2018-01-10 21:09:32 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,17 +3,27 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push("cardOne");
 
-console.log("User flipped " + cardOne);
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	};
+};
 
-var cardTwo = cards[2];
-cardsInPlay.push("cardTwo");
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId] || "User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch;
 
 if (cardsInPlay.length === 2 && cardOne === cardTwo) { 
 alert("You found a match!");
 } else	{
 	alert("Sorry, try again.");
-}
+};	
+};
+
+flipCard(0);
+flipCard(2);
+
